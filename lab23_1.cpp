b8dda1419df259df9b44565ed9c0a62eb42009ab
Mookanda <kaofang0608@gmail.com>
2026-03-06 16:17:35 +0700

    README.mdcompleted

--- lab23_1.cpp
+++ lab23_1.cpp
@@ -20,20 +20,85 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream fin("name_score.txt") ;
+    string s ;
+    
+    while(getline(fin,s)){
+        char format[] = "%[^:]: %d %d %d" ;
+        char name[100] ;
+        int a,b,c ;
+        sscanf(s.c_str(),format,name,&a,&b,&c) ;
+        
+        names.push_back(name); //เก็บชื่อ
+        
+        int total=a+b+c ;
+        scores.push_back(total);
+        grades.push_back(score2grade(total));
+        
+        
+    }
+
 
 }
 
-void getCommand(){
+void getCommand(string &command, string &key){
+    cout << "Please input your command:" ;
+    string input;
+    getline(cin, input);
+    cout << "\n" ;
+    
+    size_t pos = input.find_first_of(" "); //นับจนตัว
+    
+    if(pos != string::npos){
+        command = input.substr(0, pos); //ถ้าเจอวรรคให้commandเป็นตัวก่อนวรรค
+        key = input.substr(pos + 1); //keyเป็นตัวหลัง
+    } else {
+        command = input;
+        key = "";
+    }
+
 
 }
 
-void searchName(){
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    bool found = false;
+    cout << "---------------------------------\n";
+    
+    string upperKey = toUpperStr(key); 
 
+    for(unsigned int i = 0; i < names.size(); i++){
+        if(toUpperStr(names[i]) == upperKey){ 
+            cout << names[i] << "'s score = " << scores[i] << "\n";
+            cout << names[i] << "'s grade = " << grades[i] << "\n";
+            found = true;
+            break; 
+        }
+    }
+    
+    if(!found){
+        cout << "Cannot found.\n";
+    }
+    
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    bool found = false;
+    cout << "---------------------------------\n";
+    
+    for(unsigned int i = 0; i < grades.size(); i++){
+        if(grades[i] == key[0]){ 
+            cout << names[i] << " (" << scores[i] << ")\n";
+            found = true;
+        }
+    }
+    
+    if(!found){
+        cout << "Cannot found.\n";
+    }
+    
+    cout << "---------------------------------\n";
 }
 
 
